190ae0efd872a5a08d7439ac5357d492f5b49891
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-01-24 11:01:08 +0500

    changes added

---
+++ HelpWITHZEESHAN/my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import WaveDesign from './WeveDesign'
+
+function App() {
+  return (
+    <div className="App">
+      <WaveDesign />
+    </div>
+  );
+}
+
+export default App;
